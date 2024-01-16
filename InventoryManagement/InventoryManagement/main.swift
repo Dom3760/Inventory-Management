ef0456a690a3356d8d50002e38fd94081c7b52f2
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2024-01-16 10:26:18 -0800

    Add files via upload

---
+++ InventoryManagement/InventoryManagement/main.swift
@@ -0,0 +1,78 @@
+//
+//  main.swift
+//  InventoryManagement
+//
+//  Created by StudentAM on 1/16/24.
+//
+
+//let queue = DispatchQueue.global()
+//
+//for i in 1 ... 100000 {
+//    queue.async {
+//        usleep(useconds_t(0.5 * 1_000_000))
+//        print("Hello, World!")
+//    }
+//}
+
+
+//class Car {
+//    var brand: String
+//    var model: String
+//
+//    init(brand: String, model: String) {
+//        self.brand = brand
+//        self.model = model
+//    }
+//}
+//
+//// Creating an instance of the Car class
+//let myCar = Car(brand: "Toyota", model: "Camry")
+
+import Foundation
+
+class ceral
+{
+    
+}
+class pancakeMix
+{
+    
+}
+class Milk
+{
+    
+}
+class
+{
+    
+}
+print("What would you like to do?:")
+print("1.Add item to cart")
+print("2.Remove item from cart")
+print("3.Check item availability in stock")
+print("4.Access Admin Menu")
+print("5.Checkout")
+
+if let input1 = readLine()
+{
+    if input1 == "1"
+    {
+        print
+    }
+    if input1 == "2"
+    {
+        
+    }
+    if input1 == "3"
+    {
+        
+    }
+    if input1 == "4"
+    {
+        
+    }
+    if input1 == "5"
+    {
+        
+    }
+}
